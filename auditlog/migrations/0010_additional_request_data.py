f087b1b03cb484f71e0d2d53c8a4130702cb01d3
offensity <34161284+offensity@users.noreply.github.com>
2023-04-20 17:12:51 +0200

    Update migration file

--- auditlog/migrations/0010_additional_request_data.py
+++ auditlog/migrations/0010_additional_request_data.py
@@ -1,4 +1,3 @@
-import jsonfield.fields
 from django.db import migrations, models
 
 
@@ -12,6 +11,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="logentry",
             name="additional_request_data",
-            field=jsonfield.fields.JSONField(null=True, blank=True, verbose_name='additional request data'),
+            field=models.JSONField(null=True, blank=True, verbose_name='additional request data'),
         )
     ]
